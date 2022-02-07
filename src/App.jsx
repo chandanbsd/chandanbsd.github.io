e650d97ca56ccfc62150d2dfd123ea8613600b09
chandanbsd <49138440+chandanbsd@users.noreply.github.com>
2022-02-07 12:29:28 -0500

    Rewrite Website

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,5 @@
-import Topbar from "./components/topbar/Topbar";
-import Intro from "./components/intro/Intro"
-import Works from "./components/works/Works"
-import "./app.scss"
-import { useState } from "react";
-import Menu from "./components/menu/Menu";
-
-function App() {
-  const [menuOpen,setMenuOpen] = useState(false)
-  return (
-    <div className="app">
-     <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-     <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-     <div className="sections">
-       <Intro/>
- 
-       <Works/>
-       
-     </div>
-    </div>
-  );
-}
+const App = () => {
+  return <div>Hello world!</div>;
+};
 
 export default App;
