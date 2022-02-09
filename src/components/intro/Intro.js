475ef25efc817ba6c6fbed19107fbfabee439534
chandanbsd <49138440+chandanbsd@users.noreply.github.com>
2022-02-08 23:04:43 -0500

    CSS Responsive Update: Max Width changed to 1024px for render 2

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -7,7 +7,17 @@ const Intro = () => {
     <div className="i">
       <div className="i-left">
         <div className="i-left-wrapper">
-          <h2 className="i-intro">Hello, My name is</h2>
+          <h2 className="i-intro">
+            <SocialIcon
+              url="https://linkedin.com/in/chandanbsd"
+              target="_blank"
+            />{" "}
+            <SocialIcon url="https://github.com/chandanbsd" target="_blank" />{" "}
+            <SocialIcon url="https://twitter.com/chandanbsd" target="_blank" />{" "}
+            <SocialIcon url="https://facebook.com/bsdchandan" target="_blank" />{" "}
+            <br /> <br />
+            Hello, My name is
+          </h2>
           <h2 className="i-name">Chandan B. S. D.</h2>
           <div className="i-title">
             <div className="i-title-wrapper">
@@ -18,38 +28,6 @@ const Intro = () => {
               <div className="i-title-item">Full Stack Dev</div>
             </div>
           </div>
-          <p className="i-desc">
-            <ul>
-              <li>
-                <SocialIcon
-                  url="https://github.com/chandanbsd"
-                  target="_blank"
-                />{" "}
-                @chandanbsd
-              </li>
-              <li>
-                <SocialIcon
-                  url="https://linkedin.com/in/chandanbsd"
-                  target="_blank"
-                />{" "}
-                @chandanbsd
-              </li>
-              <li>
-                <SocialIcon
-                  url="https://twitter.com/chandanbsd"
-                  target="_blank"
-                />
-                @chandanbsd
-              </li>
-              <li>
-                <SocialIcon
-                  url="https://facebook.com/bsdchandan"
-                  target="_blank"
-                />{" "}
-                @bsdchandan
-              </li>
-            </ul>
-          </p>
         </div>
         <svg
           width="75"
