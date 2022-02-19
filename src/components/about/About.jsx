7730d33c442d68f58957d958b99f5049aa6b962e
chandanbsd <49138440+chandanbsd@users.noreply.github.com>
2022-02-19 01:03:20 -0500

    Cosmetic Update
    1. University section moved from about to intro section\n2. Font Family changed to Open-Sans\n3. Technical Skills structured in descriptive lists.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,5 @@
 import "./about.css";
 import Me2 from "../../img/me2.jpg";
-import IULogo from "../../img/iulogo.svg";
 
 const About = () => {
   return (
@@ -14,32 +13,42 @@ const About = () => {
       <div className="a-right">
         <h1 className="a-title">Technical Skills</h1>
         {/* <p className="a-sub">I'm </p> */}
-        <p className="a-desc">
-          <b>Programming Languages:</b> JavaScript, Python, Java, PHP, C#, C++,
-          C <br />
-          <b>Web Development Frameworks:</b> React.js, Express.js, Node.js,
-          Tailwind CSS, Sass, Bootstrap, CSS, HTML
-          <br />
-          <b>Mobile Application Development Frameworks:</b> React Native,
-          Flutter <br />
-          <b>Database:</b> MongoDB, MySQL <br />
-          <b>Cloud:</b> Google Firebase <br />
-          <b>End to End Automated Testing Framework:</b> Cypress <br />
-          <b>ML, Computer Vision, and Data Mining Frameworks:</b> OpenCV,
-          Mediapipe, Keras, Pandas, NumPy, Matplotlib, sci-kit-learn <br />
-        </p>
-        <div className="a-university">
-          <img src={IULogo} alt="" className="a-university-img" />
-          <div className="a-university-texts">
-            <h4 className="a-university-title">
-              Indiana University Bloomington Class of 2023 <br />
-              (M. S. in Computer Science)
-            </h4>
-            <p className="a-university-desc">
-              Luddy School of Informatics, Computing, and Engineering
-            </p>
-          </div>
-        </div>
+
+        <dl className="a-desc">
+          <dt>
+            <b>Programming Languages:</b>
+          </dt>
+          <dd>JavaScript, Python, Java, PHP, C#, C++, C</dd>
+          <dt>
+            <b>Web Development:</b>
+          </dt>
+          <dd>
+            React.js, Express.js, Node.js, Tailwind CSS, Sass, Bootstrap, CSS,
+            HTML
+          </dd>
+          <dt>
+            <b>Mobile App Development:</b>
+          </dt>
+          <dd>React Native, Flutter</dd>
+          <dt>
+            <b>Database:</b>
+          </dt>
+          <dd>MongoDB, MySQL</dd>
+          <dt>
+            <b>Cloud:</b>
+          </dt>
+          <dd>Google Firebase</dd>
+          <dt>
+            <b>End to End Automated Testing Framework:</b>
+          </dt>
+          <dd>Cypress</dd>
+          <dt>
+            <b>Machine Learning and Computer Vision:</b>
+          </dt>
+          <dd>
+            OpenCV, Mediapipe, Keras, Pandas, NumPy, Matplotlib, sci-kit-learn
+          </dd>
+        </dl>
       </div>
     </div>
   );
