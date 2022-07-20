6aff4d96aa54a97739380c1b140bfbaed4effd57
Chandan Bangalore Shankar Denanath <49138440+chandanbsd@users.noreply.github.com>
2022-07-20 01:37:59 -0400

    Website Redesign, simple and clear stylesheet

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,54 +1,62 @@
 import "./intro.css";
-import Me from "../../img/me.png";
+import Me2 from "../../img/me2.jpg";
 import { SocialIcon } from "react-social-icons";
 import IULogo from "../../img/iulogo.svg";
+import React from "react";
+import ReactIcon from "../../img/iulogo.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
 
 const Intro = () => {
   return (
-    <div className="i">
-      <div className="i-left">
-        <div className="i-left-wrapper">
-          <h2 className="i-intro">
-            <SocialIcon
-              url="https://linkedin.com/in/chandanbsd"
-              target="_blank"
-            />{" "}
-            <SocialIcon url="https://github.com/chandanbsd" target="_blank" />{" "}
-            <SocialIcon url="https://twitter.com/chandanbsd" target="_blank" />{" "}
-            <SocialIcon url="https://facebook.com/bsdchandan" target="_blank" />{" "}
-            <br /> <br />
-            Hello, My name is
-          </h2>
-          <h2 className="i-name">Chandan B. S. D.</h2>
-          <div className="i-title">
-            <div className="i-title-wrapper">
-              <div className="i-title-item">Student</div>
-              <div className="i-title-item">Software Dev</div>
-              <div className="i-title-item">Front End Dev</div>
-              <div className="i-title-item">Mobile Dev</div>
-              <div className="i-title-item">Full Stack Dev</div>
+    <React.Fragment>
+      <div className="i-social-links">
+        <div>
+          {" "}
+          <SocialIcon
+            url="https://linkedin.com/in/chandanbsd"
+            target="_blank"
+          />{" "}
+        </div>
+        <div>
+          {" "}
+          <SocialIcon
+            url="https://github.com/chandanbsd"
+            target="_blank"
+          />{" "}
+        </div>
+        <div className="resume-link">
+          Download Resume
+          <FontAwesomeIcon icon={faDownload} />
+        </div>
+      </div>
+      <div className="i">
+        <div className="i-left">
+          <div className="i-left-wrapper">
+            <h2 className="i-intro">Hello, I'm</h2>
+            <h2 className="i-name">Chandan </h2>
+            <h2 className="i-name">Bangalore Shankar Denanth</h2>
+            <div className="a-university">
+              <img src={IULogo} alt="" className="a-university-img" />
+              <div className="a-university-texts">
+                <h4 className="a-university-title">
+                  Indiana University Bloomington <br />
+                  (Master of Science in Computer Science)
+                </h4>
+                <p className="a-university-desc">Fall 2021 - Spring 2023</p>
+              </div>
             </div>
           </div>
-          <div className="a-university">
-            <img src={IULogo} alt="" className="a-university-img" />
-            <div className="a-university-texts">
-              <h4 className="a-university-title">
-                Indiana University Bloomington Class of 2023 <br />
-                (M. S. in Computer Science)
-              </h4>
-              <p className="a-university-desc">
-                Luddy School of Informatics, Computing, and Engineering
-              </p>
+        </div>
+        <div className="i-right">
+          <div className="a-left">
+            <div className="a-card">
+              <img src={Me2} alt="" className="a-img" />
             </div>
           </div>
         </div>
       </div>
-
-      <div className="i-right">
-        <div className="i-bg"></div>
-        <img src={Me} alt="Profile Picture" className="i-img" />
-      </div>
-    </div>
+    </React.Fragment>
   );
 };
 
